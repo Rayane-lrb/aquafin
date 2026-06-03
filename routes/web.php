fe26ddcc17e6c8d1d24547cc77a34063fb68e159
loicVS-cell <loicvs14@gmail.com>
2026-06-03 15:51:38 +0200

    Front End Technieker place holder

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,10 @@ Route::get('/dashboard', function () {
     return view('userzone.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/technieker', function () {
+    return view('userzone.technieker');
+})->middleware(['auth', 'verified'])->name('technieker');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [App\Http\Controllers\Userzone\ProfileController::class, 'update'])->name('profile.update');
