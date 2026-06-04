da3cfe154b10736e0c835a29f94ec547c8673ba4
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-06-04 11:24:39 +0200

    FEAT: update product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,15 +2,19 @@
 
 namespace App\Models;
 
+use Database\Factories\ProductFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    /** @use HasFactory<\Database\Factories\ProductFactory> */
+    /** @use HasFactory<ProductFactory> */
     use HasFactory;
 
-    public function category() {
+    protected $fillable = ['name', 'stock', 'is_active', 'is_flood_tool', 'product_category_id'];
+
+    public function category()
+    {
         return $this->belongsTo(ProductCategory::class);
     }
 }
