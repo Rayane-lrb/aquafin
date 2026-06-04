b425c66076e60529fa3e2a60c84148e9b3673c1a
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-06-04 21:17:52 +0200

    FEAT: order factory

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id'    => User::where('role', 'technieker')->inRandomOrder()->first()?->id
+                            ?? User::factory()->create(['role' => 'technieker'])->id,
+            'product_id' => Product::inRandomOrder()->first()?->id
+                            ?? Product::factory()->create()->id,
+            'quantity'   => $this->faker->numberBetween(1, 50),
+            'status'     => $this->faker->randomElement(['pending', 'approved', 'rejected'])
+        ];
+    }
+}
