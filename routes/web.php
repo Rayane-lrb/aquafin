9915b56ab33ac716461496a97d7b883213a46561
Rayane-lrb <rayane.laroub@student.ehb.be>
2026-06-04 10:22:56 +0200

    FEAT: productController edit function

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,5 @@ Route::get('/product', [ProductController::class, 'index'])->name('product.index
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
 Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product.edit');
 require __DIR__.'/auth.php';
