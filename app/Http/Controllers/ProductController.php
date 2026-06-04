8e74ff2f9951d4b08f25a0f46b3a0118417107ca
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-06-04 10:53:05 +0200

    FEAT: add functions to ProductController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -22,4 +22,42 @@ class ProductController extends Controller
     public function create() {
         return view('product.create');
     }
+    
+    public function store(Request $request) {
+    $request->validate([
+        'name'                => ['required', 'string', 'max:255'],
+        'stock'               => ['required', 'integer', 'min:0'],
+        'product_category_id' => ['required', 'exists:product_categories,id'],
+    ]);
+
+    Product::create($request->only(['name', 'stock', 'is_active', 'is_flood_tool', 'product_category_id']));
+
+    return redirect()->route('product.index');
+}
+
+public function edit(string $id) {
+    $product = Product::findOrFail($id);
+
+    return view('product.edit', ['product' => $product]);
+}
+
+public function update(Request $request, string $id) {
+    $request->validate([
+        'name'                => ['required', 'string', 'max:255'],
+        'stock'               => ['required', 'integer', 'min:0'],
+        'product_category_id' => ['required', 'exists:product_categories,id'],
+    ]);
+
+    $product = Product::findOrFail($id);
+    $product->update($request->only(['name', 'stock', 'is_active', 'is_flood_tool', 'product_category_id']));
+
+    return redirect()->route('product.index');
+}
+
+public function destroy(string $id) {
+    $product = Product::findOrFail($id);
+    $product->delete();
+
+    return redirect()->route('product.index');
+}
 }
