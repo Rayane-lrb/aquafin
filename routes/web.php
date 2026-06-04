4150a90dee782fe82d8ef6a998cb942695ac9d03
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-06-04 12:03:10 +0200

    PERF: automatic direct login page

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\OrderController;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
