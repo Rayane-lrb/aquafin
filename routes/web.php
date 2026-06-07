4336e03321eb98dcd0d4839a214645ac72cc6c95
Houdeyfa Oulad Ben Taïb <145970301+hdyfaobt@users.noreply.github.com>
2026-06-07 16:52:36 +0200

    REFACTOR: rewrite routes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\UserController as AdminUserController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductCategoryController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Userzone\ProfileController;
 use App\Http\Controllers\Userzone\SuggestionController;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -39,40 +39,42 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// Product categories
-Route::get('/productcategory', [ProductCategoryController::class, 'index'])->name('productcategory.index');
-Route::get('/productcategory/create', [ProductCategoryController::class, 'create'])->name('productcategory.create');
-Route::post('/productcategory', [ProductCategoryController::class, 'store'])->name('productcategory.store');
-Route::get('/productcategory/{id}/edit', [ProductCategoryController::class, 'edit'])->name('productcategory.edit');
-Route::patch('/productcategory/{id}', [ProductCategoryController::class, 'update'])->name('productcategory.update');
-Route::delete('/productcategory/{id}', [ProductCategoryController::class, 'destroy'])->name('productcategory.destroy');
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Product categories
+    Route::get('/productcategory', [ProductCategoryController::class, 'index'])->name('productcategory.index');
+    Route::get('/productcategory/create', [ProductCategoryController::class, 'create'])->name('productcategory.create');
+    Route::post('/productcategory', [ProductCategoryController::class, 'store'])->name('productcategory.store');
+    Route::get('/productcategory/{id}/edit', [ProductCategoryController::class, 'edit'])->name('productcategory.edit');
+    Route::patch('/productcategory/{id}', [ProductCategoryController::class, 'update'])->name('productcategory.update');
+    Route::delete('/productcategory/{id}', [ProductCategoryController::class, 'destroy'])->name('productcategory.destroy');
 
-// Products
-Route::get('/product', [ProductController::class, 'index'])->name('product.index');
-Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
-Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
-Route::post('/product', [ProductController::class, 'store'])->name('product.store');
-Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product.edit');
-Route::patch('/product/{id}', [ProductController::class, 'update'])->name('product.update');
-Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
+    // Products
+    Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/product/create', [ProductController::class, 'create'])->name('product.create');
+    Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+    Route::post('/product', [ProductController::class, 'store'])->name('product.store');
+    Route::get('/product/{id}/edit', [ProductController::class, 'edit'])->name('product.edit');
+    Route::patch('/product/{id}', [ProductController::class, 'update'])->name('product.update');
+    Route::delete('/product/{id}', [ProductController::class, 'destroy'])->name('product.destroy');
 
-// Orders
-Route::get('/order', [OrderController::class, 'index'])->name('order.index');
-Route::get('/order/create', [OrderController::class, 'create'])->name('order.create');
-Route::post('/order', [OrderController::class, 'store'])->name('order.store');
-Route::get('/order/{id}/edit', [OrderController::class, 'edit'])->name('order.edit');
-Route::patch('/order/{id}', [OrderController::class, 'update'])->name('order.update');
-Route::patch('/order/{id}/approve', [OrderController::class, 'approve'])->name('order.approve');
-Route::patch('/order/{id}/reject', [OrderController::class, 'reject'])->name('order.reject');
+    // Orders
+    Route::get('/order', [OrderController::class, 'index'])->name('order.index');
+    Route::get('/order/create', [OrderController::class, 'create'])->name('order.create');
+    Route::post('/order', [OrderController::class, 'store'])->name('order.store');
+    Route::get('/order/{id}/edit', [OrderController::class, 'edit'])->name('order.edit');
+    Route::patch('/order/{id}', [OrderController::class, 'update'])->name('order.update');
+    Route::patch('/order/{id}/approve', [OrderController::class, 'approve'])->name('order.approve');
+    Route::patch('/order/{id}/reject', [OrderController::class, 'reject'])->name('order.reject');
 
-// Suggestions
-Route::get('/suggestion', [SuggestionController::class, 'index'])->name('suggestion.index');
-Route::get('/suggestion/create', [SuggestionController::class, 'create'])->name('suggestion.create');
-Route::get('/suggestion/{id}', [SuggestionController::class, 'show'])->name('suggestion.show');
-Route::post('/suggestion', [SuggestionController::class, 'store'])->name('suggestion.store');
-Route::patch('/suggestion/{id}/approve', [SuggestionController::class, 'approve'])->name('suggestion.approve');
-Route::patch('/suggestion/{id}/reject', [SuggestionController::class, 'reject'])->name('suggestion.reject');
-Route::delete('/suggestion/{id}', [SuggestionController::class, 'destroy'])->name('suggestion.destroy');
+    // Suggestions
+    Route::get('/suggestion', [SuggestionController::class, 'index'])->name('suggestion.index');
+    Route::get('/suggestion/create', [SuggestionController::class, 'create'])->name('suggestion.create');
+    Route::get('/suggestion/{id}', [SuggestionController::class, 'show'])->name('suggestion.show');
+    Route::post('/suggestion', [SuggestionController::class, 'store'])->name('suggestion.store');
+    Route::patch('/suggestion/{id}/approve', [SuggestionController::class, 'approve'])->name('suggestion.approve');
+    Route::patch('/suggestion/{id}/reject', [SuggestionController::class, 'reject'])->name('suggestion.reject');
+    Route::delete('/suggestion/{id}', [SuggestionController::class, 'destroy'])->name('suggestion.destroy');
+});
 
 // Admin - User management
 Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
