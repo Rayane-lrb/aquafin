aa6769f0587246d797cbe0a05e7aaee3c331f514
loicVS-cell <loicvs14@gmail.com>
2026-06-11 14:09:13 +0200

    feat: red outline for hidden product

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -47,7 +47,7 @@
     @else
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
         @foreach ($products as $product)
-        <div class="bg-white rounded-xl shadow-sm overflow-hidden flex flex-col">
+        <div class="bg-white rounded-xl shadow-sm overflow-hidden flex flex-col {{ !$product->is_active ? 'border-2 border-red-400' : '' }}">
 
             <div class="{{ !$product->is_active ? 'opacity-50' : '' }}">
             @if ($product->image)
