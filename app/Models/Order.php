d35285dfa4b23cd59ee600ba2560117dc79f398f
Houdé <145970301+hdyfaobt@users.noreply.github.com>
2026-06-11 15:35:20 +0200

    FEAT: AJOUT URGENT, ORDER_GROUP_ID ET DELIVERY_DATE DANS FILLABLE

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = ['user_id', 'product_id', 'quantity', 'status', 'warehouse_id'];
+    protected $fillable = ['order_group_id', 'user_id', 'product_id', 'quantity', 'status', 'warehouse_id', 'delivery_date', 'urgent'];
 
     public function user()
     {
