8bf605c10ad10a9787d74fec14c0b289a0034d7d
loicVS-cell <loicvs14@gmail.com>
2026-06-11 18:45:44 +0200

    fix: removing the Magazijn option from Technieker

--- resources/views/layouts/app_navigation.blade.php
+++ resources/views/layouts/app_navigation.blade.php
@@ -24,7 +24,7 @@
         ['route' => 'suggestion.*',     'href' => route('suggestion.index'),     'label' => 'Suggesties',  'icon' => 'M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z'],
         ['route' => 'neerslag.*',       'href' => route('neerslag.index'),       'label' => 'Neerslag',    'icon' => 'M3 15a4 4 0 004 4h9a5 5 0 10-.1-9.999 5.002 5.002 0 10-9.78 2.096A4.001 4.001 0 003 15z'],
         ['route' => 'productcategory.*','href' => route('productcategory.index'),'label' => 'Categorieën', 'icon' => 'M7 7h.01M7 3h5l6 6v11a2 2 0 01-2 2H5a2 2 0 01-2-2V5a2 2 0 012-2z'],
-        ...($role !== 'magazijnBeheerder' ? [['route' => 'warehouse.*', 'href' => route('warehouse.index'), 'label' => 'Magazijnen', 'icon' => 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4']] : []),
+        ...($role === 'admin' ? [['route' => 'warehouse.*', 'href' => route('warehouse.index'), 'label' => 'Magazijnen', 'icon' => 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4']] : []),
     ];
 
     if ($role === 'admin') {
