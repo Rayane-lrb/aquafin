eae99aa8b59364eafe88196eb3d86ebd4fdf59e4
Houdé <145970301+hdyfaobt@users.noreply.github.com>
2026-06-11 14:09:10 +0200

    FIX: buttons

--- resources/views/layouts/app_navigation.blade.php
+++ resources/views/layouts/app_navigation.blade.php
@@ -15,7 +15,7 @@
         default             => 'Gebruiker',
     };
 
-    $cartCount = count(session('cart', []));
+    $cartCount = array_sum(session('cart', []));
 
     $nav = [
         ['route' => 'product.*',        'href' => route('product.index'),        'label' => 'Catalogus',   'icon' => 'M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10'],
